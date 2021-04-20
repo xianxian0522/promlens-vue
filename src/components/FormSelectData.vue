70559ce73d12b51246c2286f548da6f6fefd2c35
suixian.gong <suixian.gong@sumscope.com>
2021-04-20 18:30:13 +0800

    form

---
+++ src/components/FormSelectData.vue
@@ -0,0 +1,13 @@
+<template>
+<div></div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "FormSelectData"
+}
+</script>
+
+<style scoped lang="less">
+@import "index.less";
+</style>
---
+++ src/components/FormSubquery.vue
@@ -0,0 +1,13 @@
+<template>
+<div></div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "FormSubquery"
+}
+</script>
+
+<style scoped lang="less">
+@import "index.less";
+</style>
