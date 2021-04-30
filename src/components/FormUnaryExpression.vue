0c0661d059b3743a0b55aefe61721b91a3650715
suixian.gong <suixian.gong@sumscope.com>
2021-04-30 15:52:56 +0800

    unary

--- src/components/FormUnaryExpression.vue
+++ src/components/FormUnaryExpression.vue
@@ -12,25 +12,55 @@
       </a-select>
     </a-form-item>
   </a-form>
+  <a-button class="btn btn-secondary btn-sm" @click="onSubmit">
+    <CheckOutlined />Apply changes
+  </a-button>
 </div>
 </template>
 
 <script lang="ts">
-import {reactive} from "vue";
+import {inject, onUnmounted, reactive, watch} from "vue";
+import {CheckOutlined} from "@ant-design/icons-vue";
 
 export default {
   name: "FormUnaryExpression",
   props: ['unaryExpr'],
+  components: {CheckOutlined},
   emits: ['previewChange'],
   setup(props, content) {
+    const updateExprValue: any = inject('updateExprValue')
+    const updateExprIndex: number | undefined = inject('updateExprIndex')
+    const updateLeft = inject('updateLeft')
     const formState = reactive({
       unaryOp: props.unaryExpr?.unaryOp || '-',
       expr: props.unaryExpr?.expr
     })
-    content.emit('previewChange', formState)
+
+    const getUnary = () => {
+      const data: any = {
+        unaryOp: formState.unaryOp,
+        expr: formState.expr,
+      }
+      return data
+    }
+
+    const onSubmit = () => {
+      const value = {
+        unaryExpr: getUnary(),
+        showLeft: updateLeft,
+      }
+      updateExprValue([value, 'unaryExpr', updateExprIndex])
+    }
+
+    content.emit('previewChange', props.unaryExpr)
+
+    watch(formState, () => {
+      content.emit('previewChange', getUnary())
+    })
 
     return {
       formState,
+      onSubmit,
     }
   }
 }
--- src/components/PreviewUnary.vue
+++ src/components/PreviewUnary.vue
@@ -15,7 +15,7 @@ export default {
   name: "PreviewUnary",
   props: ['preview'],
   setup(props) {
-
+    console.log(props, ';;;;;;;;;')
   }
 }
 </script>
