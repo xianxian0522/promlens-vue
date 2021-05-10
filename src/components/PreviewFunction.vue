8ada21be40d4b7c00499a6d501e3f9009933489c
suixian.gong <suixian.gong@sumscope.com>
2021-05-10 16:19:37 +0800

    function preview

--- src/components/PreviewFunction.vue
+++ src/components/PreviewFunction.vue
@@ -3,7 +3,8 @@
   <span class="promql-keyword">{{ preview ? preview?.functionIdentifier : 'rate' }}</span><span class="ast-parens">(</span>
   <span v-if="preview?.functionArgs">
     <span v-for="(item, index) in preview?.functionArgs" :key="index">
-      <PreviewSelectData v-if="item.vectorSelector" :preview="item.vectorSelector" />
+<!--      <PreviewSelectData v-if="item.vectorSelector" :preview="item.vectorSelector" />-->
+      <PreviewExpr :preview="item" />
     </span>
   </span>
   <span v-else>
@@ -20,7 +21,8 @@ export default {
   name: "PreviewFunction",
   props: ['preview'],
   components: {
-    PreviewSelectData: defineAsyncComponent(() => import('../components/PreviewSelectData.vue'))
+    // PreviewSelectData: defineAsyncComponent(() => import('./PreviewSelectData.vue')),
+    PreviewExpr: defineAsyncComponent(() => import('./PreviewExpr.vue'))
   },
   setup(props: any) {
     console.log(props, 'function prew')
