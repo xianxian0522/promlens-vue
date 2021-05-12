40acee3dd29ffc41139f6894a4351fec126b4b9f
suixian.gong <suixian.gong@sumscope.com>
2021-05-12 10:06:52 +0800

    code mirror

--- src/views/PromQL.vue
+++ src/views/PromQL.vue
@@ -9,7 +9,8 @@
           <div class="expression-input-wrapper">
             <div class="expression-input expression-input-stale">
               <a-button class="expression-input-sync-btn btn btn-light btn-sm"><SyncOutlined /></a-button>
-              <div class="cm-expression-input" id="editor"></div>
+<!--              <div class="cm-expression-input" :id="'editor' + index"></div>-->
+              <PromQLCodeMirror :codeId="index" />
               <div class="expression-input-enter">
                 <EnterOutlined />
               </div>
@@ -48,15 +49,12 @@ import Expr from "@/views/Expr.vue";
 import {onMounted, reactive, ref, toRefs} from "vue";
 import {CloseOutlined, EnterOutlined, PlusOutlined, ReadOutlined, SyncOutlined} from "@ant-design/icons-vue";
 import {metricNameData, promRepository} from "@/api/promRepository";
-import {PromQLExtension} from "codemirror-promql";
-import {EditorView, keymap, placeholder, ViewUpdate} from "@codemirror/view";
-import {EditorState, Prec} from "@codemirror/state";
-import {basicSetup} from "@codemirror/basic-setup";
-import {HighlightStyle, tags} from "@codemirror/highlight";
+import PromQLCodeMirror from "@/views/PromQLCodeMirror.vue";
 
 export default {
   name: "PromQL",
   components: {
+    PromQLCodeMirror,
     Expr,
     ReadOutlined,
     CloseOutlined,
@@ -161,86 +159,6 @@ export default {
       ql: [{expr: {}}] as PromQL[],
     })
     console.log(state.ql);
-    const exprValue = ref('node_arp_entries')
-    const promQL = new PromQLExtension().setComplete({
-      remote: {
-        url: 'http://prometheus.proxy.sumscope.com:8000',
-      }
-    })
-    const promQLHighlightMaterialTheme = HighlightStyle.define([
-      {
-        tag: tags.deleted,
-        textDecoration: 'line-through',
-      },
-      {
-        tag: tags.inserted,
-        textDecoration: 'underline',
-      },
-      {
-        tag: tags.link,
-        textDecoration: 'underline',
-      },
-      {
-        tag: tags.strong,
-        fontWeight: 'bold',
-      },
-      {
-        tag: tags.emphasis,
-        fontStyle: 'italic',
-      },
-      {
-        tag: tags.invalid,
-        color: '#f00',
-      },
-      {
-        tag: tags.keyword,
-        color: '#C792EA',
-      },
-      {
-        tag: tags.operator,
-        color: '#89DDFF',
-      },
-      {
-        tag: tags.atom,
-        color: '#F78C6C',
-      },
-      {
-        tag: tags.number,
-        color: '#FF5370',
-      },
-      {
-        tag: tags.string,
-        color: '#99b867',
-      },
-      {
-        tag: [tags.escape, tags.regexp],
-        color: '#e40',
-      },
-      {
-        tag: tags.definition(tags.variableName),
-        color: '#f07178',
-      },
-      {
-        tag: tags.labelName,
-        color: '#f07178',
-      },
-      {
-        tag: tags.typeName,
-        color: '#085',
-      },
-      {
-        tag: tags.function(tags.variableName),
-        color: '#C792EA',
-      },
-      {
-        tag: tags.definition(tags.propertyName),
-        color: '#00c',
-      },
-      {
-        tag: tags.comment,
-        color: '#546e7a',
-      }
-    ])
 
     const updateValue = (value) => {
       const [v, str, index, qlIndex] = value;
@@ -273,42 +191,7 @@ export default {
       await promRepository.queryLabel()
     }
 
-    const parseExpr = (v) => {
-      console.log(exprValue.value, '=======', v.state.doc)
-    }
-
     onMounted(() => {
-      const doc = document.getElementById('editor')
-      if (doc !== null) {
-        new EditorView({
-          state: EditorState.create({
-            extensions: [
-              basicSetup,
-              promQL.asExtension(),
-              EditorView.lineWrapping,
-              promQLHighlightMaterialTheme,
-              placeholder('Enter query or edit tree view below...'),
-              Prec.override(
-                  keymap.of([
-                    {
-                      key: 'Enter',
-                      run: (v: EditorView): boolean => {
-                        parseExpr(v)
-                        return true
-                      }
-                    }
-                  ])
-              ),
-              // EditorView.updateListener.of((update: ViewUpdate) => {
-              //   console.log('xxx', update.state.doc)
-              // })
-            ],
-            doc: exprValue.value,
-          }),
-          parent: doc,
-        })
-      }
-
       queryLabelValue()
       queryLabels()
     })
@@ -326,32 +209,4 @@ export default {
 
 <style lang="less">
 @import '../components/index.less';
-.cm-completionIcon {
-  font-size: 16px !important;
-  line-height: 1;
-  margin-right: 10px;
-  vertical-align: top;
-  font-family: codicon;
-  padding-right: 0 !important;
-  opacity: 1 !important;
-  color: #007acc;
-}
-.cm-completionMatchedText {
-  color: #0066bf;
-}
-.cm-completionDetail {
-  float: right;
-  color: #999;
-}
-// 不显示几行
-.cm-gutters {
-  display: none !important;
-}
-.cm-activeLine {
-  background-color: transparent !important;
-}
-// 去掉虚线
-.ͼ1.cm-focused {
-  outline: 0
-}
 </style>
---
+++ src/views/PromQLCodeMirror.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="cm-expression-input" :id="'editor' + codeId"></div>
+</template>
+
+<script lang="ts">
+import {onMounted, ref} from "vue";
+import {PromQLExtension} from "codemirror-promql";
+import {HighlightStyle, tags} from "@codemirror/highlight";
+import {EditorView, keymap, placeholder, ViewUpdate} from "@codemirror/view";
+import {EditorState, Prec} from "@codemirror/state";
+import {basicSetup} from "@codemirror/basic-setup";
+
+export default {
+  name: "PromQLCodeMirror",
+  props: ['codeId'],
+  setup(props) {
+    const exprValue = ref('node_arp_entries')
+    const promQL = new PromQLExtension().setComplete({
+      remote: {
+        url: 'http://prometheus.proxy.sumscope.com:8000',
+      }
+    })
+    const promQLHighlightMaterialTheme = HighlightStyle.define([
+      {
+        tag: tags.deleted,
+        textDecoration: 'line-through',
+      },
+      {
+        tag: tags.inserted,
+        textDecoration: 'underline',
+      },
+      {
+        tag: tags.link,
+        textDecoration: 'underline',
+      },
+      {
+        tag: tags.strong,
+        fontWeight: 'bold',
+      },
+      {
+        tag: tags.emphasis,
+        fontStyle: 'italic',
+      },
+      {
+        tag: tags.invalid,
+        color: '#f00',
+      },
+      {
+        tag: tags.keyword,
+        color: '#C792EA',
+      },
+      {
+        tag: tags.operator,
+        color: '#89DDFF',
+      },
+      {
+        tag: tags.atom,
+        color: '#F78C6C',
+      },
+      {
+        tag: tags.number,
+        color: '#FF5370',
+      },
+      {
+        tag: tags.string,
+        color: '#99b867',
+      },
+      {
+        tag: [tags.escape, tags.regexp],
+        color: '#e40',
+      },
+      {
+        tag: tags.definition(tags.variableName),
+        color: '#f07178',
+      },
+      {
+        tag: tags.labelName,
+        color: '#f07178',
+      },
+      {
+        tag: tags.typeName,
+        color: '#085',
+      },
+      {
+        tag: tags.function(tags.variableName),
+        color: '#C792EA',
+      },
+      {
+        tag: tags.definition(tags.propertyName),
+        color: '#00c',
+      },
+      {
+        tag: tags.comment,
+        color: '#546e7a',
+      }
+    ])
+
+    const parseExpr = (v) => {
+      console.log(exprValue.value, '=======', v.state.doc)
+    }
+
+    const inputExpr = (codeId) => {
+      const doc = document.getElementById('editor' + codeId)
+      if (doc !== null) {
+        new EditorView({
+          state: EditorState.create({
+            extensions: [
+              basicSetup,
+              promQL.asExtension(),
+              EditorView.lineWrapping,
+              promQLHighlightMaterialTheme,
+              placeholder('Enter query or edit tree view below...'),
+              Prec.override(
+                  keymap.of([
+                    {
+                      key: 'Enter',
+                      run: (v: EditorView): boolean => {
+                        parseExpr(v)
+                        return true
+                      }
+                    }
+                  ])
+              ),
+              // EditorView.updateListener.of((update: ViewUpdate) => {
+              //   console.log('xxx', update.state.doc)
+              // })
+            ],
+            doc: exprValue.value,
+          }),
+          parent: doc,
+        })
+      }
+    }
+
+    onMounted(() => {
+      inputExpr(props.codeId)
+    })
+
+    return {
+
+    }
+
+  }
+}
+</script>
+
+<style lang="less">
+@import "../components/index.less";
+.cm-completionIcon {
+  font-size: 16px !important;
+  line-height: 1;
+  margin-right: 10px;
+  vertical-align: top;
+  font-family: codicon;
+  padding-right: 0 !important;
+  opacity: 1 !important;
+  color: #007acc;
+}
+.cm-completionMatchedText {
+  color: #0066bf;
+}
+.cm-completionDetail {
+  float: right;
+  color: #999;
+}
+// 不显示几行
+.cm-gutters {
+  display: none !important;
+}
+.cm-activeLine {
+  background-color: transparent !important;
+}
+// 去掉虚线
+.ͼ1.cm-focused {
+  outline: 0
+}
+</style>
